9346e0256ad7100463853b1fc5622541eccd98e1
Kee <codinginpain@gmail.com>
2023-03-30 22:58:19 +0900

    D-Day 기능 완성

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,23 +9,96 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime(
+    2021,
+    3,
+    9,
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.pink[100],
       body: SafeArea(
+        bottom: false,
         child: SizedBox(
           width: MediaQuery.of(context).size.width,
           child: Column(
             children: [
-              const Text(
-                "Su & Kee",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'Alkatra',
-                  fontSize: 80,
-                ),
+              _TopPart(
+                selectedDate: selectedDate,
+                onPressed: onheartPressed,
               ),
+              const _BottomPart(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  onheartPressed() {
+    final now = DateTime(
+      DateTime.now().year,
+      DateTime.now().month,
+      DateTime.now().day,
+    );
+    showCupertinoDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            color: Colors.white,
+            height: 300,
+            child: CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.date,
+              initialDateTime: selectedDate,
+              // initialDateTime:
+              // DateTime(now.year, now.month, now.day - 1),
+              maximumDate: DateTime(now.year, now.month, now.day),
+              onDateTimeChanged: (DateTime date) {
+                setState(() {
+                  selectedDate = date;
+                });
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _TopPart extends StatelessWidget {
+  final DateTime selectedDate;
+  final VoidCallback onPressed;
+  const _TopPart({
+    required this.selectedDate,
+    required this.onPressed,
+    Key? key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final now = DateTime(
+      DateTime.now().year,
+      DateTime.now().month,
+      DateTime.now().day,
+    );
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          const Text(
+            "Su & Kee",
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'Alkatra',
+              fontSize: 80,
+            ),
+          ),
+          Column(
+            children: [
               const Text(
                 "우리 처음 만난 날",
                 style: TextStyle(
@@ -33,27 +107,50 @@ class _HomeScreenState extends State<HomeScreen> {
                   fontSize: 30,
                 ),
               ),
-              const Text(
-                "2021.3.0",
-                style: TextStyle(
+              Text(
+                "${selectedDate.year}.${selectedDate.month}.${selectedDate.day}",
+                style: const TextStyle(
                     color: Colors.white, fontFamily: 'Alkatra', fontSize: 20),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.favorite),
-              ),
-              const Text(
-                "D + 1",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'Alkatra',
-                  fontSize: 50,
-                  fontWeight: FontWeight.w700,
-                ),
               ),
             ],
           ),
-        ),
+          IconButton(
+            iconSize: 60.0,
+            onPressed: onPressed,
+            icon: const Icon(
+              Icons.favorite,
+              color: Colors.red,
+            ),
+          ),
+          Text(
+            "D + ${DateTime(
+                  now.year,
+                  now.month,
+                  now.day,
+                ).difference(selectedDate).inDays + 1}",
+            style: const TextStyle(
+              color: Colors.white,
+              fontFamily: 'Alkatra',
+              fontSize: 50,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BottomPart extends StatelessWidget {
+  const _BottomPart({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Image.asset(
+        'assets/images/sukee.jpeg',
       ),
     );
   }
